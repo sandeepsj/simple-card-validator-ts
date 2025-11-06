cf440322edb6863931bb8e552da4977c450a75e5
sandeep.s <sandeep.s@juspay.in>
2025-11-06 18:35:50 +0530

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 /**
  * Simple Card Validator TypeScript Library
  *
- * This internal index file exports the CardValidator class
+ * This internal index file exports the CardValidator class and types
  */
 
 // Export the CardValidator class
 export { default } from './validators';
+
+// Export types for TypeScript consumers
+export type { CardDetails, CardType } from './types';
